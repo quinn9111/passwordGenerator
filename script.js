b4e90a9b8246b5d18df57d2887a785e9d3869090
Jessica Medina <lic.jessica.medina@gmail.com>
2022-08-28 20:08:28 -0500

    Remaining conditions

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 // Assignment code here
+
+  var responsePassword = window.confirm("Should the password include lowercase, uppercase, numeric, and/or special characters");
+  if (responsePassword) {
+   var charsResponse = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  else
+  var charsResponse = "0123456789abcdefghijklmnopqrstuvwxyz";
+  ;
+
+  var generateBtn = document.querySelector("#generate");
+  var lengthPassword = window.prompt("Insert password lenght between 8 characters and no more than 128 characters");
+  lengthPassword = parseInt(lengthPassword);
+  
+  if (lengthPassword>128 ){
+  alert("Please insert a value less than 128");
+  generateBtn.disabled=true;
+}
+
+else if (lengthPassword<8){
+  alert("Please insert a value greater than 8");
+  generateBtn.disabled=true;
+}
+
+
 function generatePassword() {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
+  var chars = charsResponse;
+  var promptLenght = lengthPassword-1;
   var password = "";
-for (var i = 0; i <= passwordLength; i++) {
+for (var i = 0; i <= promptLenght; i++) {
  var randomNumber = Math.floor(Math.random() * chars.length);
  password += chars.substring(randomNumber, randomNumber +1);
 }
@@ -18,6 +42,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 
 }
 
