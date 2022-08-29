80648869817c2d1e4c0e85498cfef4074a91d619
Jessica Medina <lic.jessica.medina@gmail.com>
2022-08-28 21:48:43 -0500

    Polish window prompt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-  var responsePassword = window.confirm("Should the password include lowercase, uppercase, numeric, and/or special characters");
+  var responsePassword = window.confirm("Should the password include lowercase, uppercase, numeric, and/or special characters?");
   if (responsePassword) {
    var charsResponse = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
@@ -9,19 +9,18 @@
   ;
 
   var generateBtn = document.querySelector("#generate");
-  var lengthPassword = window.prompt("Insert password lenght between 8 characters and no more than 128 characters");
+  var lengthPassword = window.prompt("Please insert numeric length of password between 8 to 128 characters");
   lengthPassword = parseInt(lengthPassword);
   
   if (lengthPassword>128 ){
-  alert("Please insert a value less than 128");
+  alert("Insert a numeric value less than 128");
   generateBtn.disabled=true;
 }
 
 else if (lengthPassword<8){
-  alert("Please insert a value greater than 8");
+  alert("Insert a numeric value greater than 8");
   generateBtn.disabled=true;
 }
-
 
 function generatePassword() {
   var chars = charsResponse;
@@ -42,8 +41,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-
 }
 
 // Add event listener to generate button
